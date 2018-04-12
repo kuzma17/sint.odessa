8e099e64c2237f5a6df3a3b433337abe99c6c936
kuzma <v.kuzma@mail.ru>
2018-04-12 10:31:51 +0300

    Login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,100 @@
 @extends('layouts.container3')
-
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Авторизация</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Авторизация</div>
+                    <div class="panel-body">
+                        <!--<div class="form-group" style="border-bottom: 1px solid #cccccc">
+                            <label for="email" class="col-md-4 control-label">Авторизация через соцсети</label>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <div class="col-md-6" style="padding: 6px">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <div class="social-buttons2">
+                                    <a href="{{ url('/social/facebook') }}" class="btn btn-fb"><i
+                                                class="fa fa-facebook"></i> Facebook</a>
+                                    <a href="{{ url('/social/twitter') }}" class="btn btn-tw"><i
+                                                class="fa fa-twitter"></i> Twitter</a>-->
+                                <!-- <a href="{{ url('/social/odniklassniki') }}" class="btn btn-od"><i class="fa fa-odnoklassniki"></i> Odniklassniki</a>
+                                    <a href="{{ url('/social/vkontakte') }}" class="btn btn-vk"><i class="fa fa-vk"></i> Vkontakte</a>
+                                    <a href="{{ url('/social/mailru') }}" class="btn btn-mr"><i class="fa fa-at"></i> Mail.ru</a> -->
+                                    <!--<a href="{{ url('/social/google') }}" class="btn btn-go"><i
+                                                class="fa fa-google"></i> Google</a>
+                                    <a href="{{ url('/social/github') }}" class="btn btn-git"><i class="fa fa-github"
+                                                                                                 aria-hidden="true"></i>
+                                        Github</a>
+                                </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+                            </div>
+                            <div class="clear"></div>
+                        </div>-->
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email"
+                                           value="{{ old('email') }}" required autofocus>
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Пароль</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Запомнить меня
-                                    </label>
+                                    @endif
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Пароль</label>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Забыли пароль?
-                                </a>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
                             </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="remember"> Запомнить пароль
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Вход
+                                    </button>
+
+                                    <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                                        Забыли пароль?
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    @if(Request::path() != 'login_admin')
+                        <div class="panel-heading" style="border-top: 1px solid #cccccc">
+                        <!-- Впервые здесь ? <a style="float: right" href="{{ url('/register') }}"><b>Регистрация</b></a> -->
+                            <div class="social-buttons2 col-md-6 col-lg-offset-4">
+                                <a href="{{ url('/register') }}" class="btn btn-od"><i class="fa fa-at"></i> <b>Регистрация</b></a>
+                            </div>
+                            <div style="clear: both"></div>
                         </div>
-                    </form>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
