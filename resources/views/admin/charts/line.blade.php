10f6b5a7475959111d30d7dd5757f04d6d8b72a3
kuzma <v.kuzma@mail.ru>
2018-05-07 23:48:41 +0300

    chart line

--- resources/views/admin/charts/line.blade.php
+++ resources/views/admin/charts/line.blade.php
@@ -1,25 +1,18 @@
-<canvas id="ChartLine" width="400" height="200"></canvas>
+<canvas id="{{$id}}" width="400" height="200"></canvas>
 <script>
     $(function () {
-        var ctx = document.getElementById("ChartLine").getContext('2d');
+        var ctx = document.getElementById("{{$id}}").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: {!! $data['labelArray'] !!},
                 datasets: [
                     {
-                        label: "Visitors",
+                        label: "Orders",
                         borderColor: "rgba(220,220,220,1)",
                         borderWidth: 1,
                         backgroundColor: "rgba(0,255,0,0.2)",
                         data: {!! $data['dataArray'] !!}
-                    },
-                    {
-                        label: "Page Views",
-                        borderColor: "rgba(220,220,220,1)",
-                        borderWidth: 1,
-                        backgroundColor: "rgba(255,255,0,0.2)",
-                        data: {!! $data['data1Array'] !!}
                     }
                 ]
             },
