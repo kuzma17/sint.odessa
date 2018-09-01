a1d8bca0ef7a301fc7a6d8b472fe91ac1db2d43a
kuzma <v.kuzma@mail.ru>
2018-09-01 13:16:48 +0300

    On Jivosite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -245,12 +245,10 @@
 <script type="text/javascript" src="http://mvcreative.ru/example/6/2/snow.js"></script>
 -->
 <!-- BEGIN JIVOSITE CODE {literal} -->
-<!--
 <script type='text/javascript'>
 (function(){ var widget_id = 'zu8O4tOtH7';var d=document;var w=window;function l(){
 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 </script>
--->
 <!-- {/literal} END JIVOSITE CODE -->
 </body>
 </html>
