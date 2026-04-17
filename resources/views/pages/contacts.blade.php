7c28e74d6b654b0a7fd7f11b5a08624ae6605cdb
kuzma <v.kuzma@mail.ru>
2026-04-17 14:22:34 +0300

    css contacts
    # Conflicts:
    #	public/sitemap.xml
    #	public/vendor/laravel-admin/bootstrap-fileinput/js/plugins/piexif.js
    #	public/vendor/laravel-admin/bootstrap-fileinput/js/plugins/piexif.min.js
    #	public/vendor/laravel-admin/sweetalert2/dist/sweetalert2.css
    #	public/vendor/laravel-admin/sweetalert2/dist/sweetalert2.min.js
    #	resources/lang/ar/admin.php
    #	resources/lang/az/admin.php
    #	resources/lang/fa/admin.php
    #	resources/lang/id/admin.php
    #	resources/lang/ja/admin.php
    #	resources/lang/ko/admin.php
    #	resources/lang/ms/admin.php
    #	resources/lang/pt-BR/admin.php
    #	resources/lang/pt/admin.php
    #	resources/lang/tr/admin.php
    #	resources/lang/uk/admin.php

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -13,107 +13,286 @@
         </div>
     </section>
 
-    <section class="section">
+{{--    <section class="section">--}}
+{{--        <div class="container">--}}
+{{--            <h2 class="text-center mb-5">Наши сервисные центры</h2>--}}
+{{--            <div class="section-divider"></div>--}}
+{{--            <div class="row g-4">--}}
+
+{{--                <!-- Филиал -->--}}
+{{--                <div class="col-md-6 col-lg-6">--}}
+{{--                    <div class="card-box contact-office-card btn-office" data-lat="46.482146" data-lng="30.730281">--}}
+
+{{--                        <h4>Центр (Соборка)</h4>--}}
+{{--                        <p>--}}
+{{--                            Сервисный центр расположен в центральной части Одессы--}}
+{{--                            и удобен для клиентов из делового центра города.--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-location-dot"></i>--}}
+{{--                            Соборная площадь, 12--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-phone"></i>--}}
+{{--                            +38 (048) XXX-XX-XX--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-map"></i> показать на карте--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <!-- Филиал -->--}}
+
+{{--                <div class="col-md-6 col-lg-6">--}}
+{{--                    <div class="card-box contact-office-card btn-office" data-lat="46.43711" data-lng="30.730315">--}}
+{{--                        <h4>Черемушки</h4>--}}
+{{--                        <p>--}}
+{{--                            Филиал обслуживает клиентов Малиновского района--}}
+{{--                            и прилегающих кварталов.--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-location-dot"></i>--}}
+{{--                            Адмиральский проспект, 33а--}}
+{{--                        </p>--}}
+
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-phone"></i>--}}
+{{--                            +38 (048) XXX-XX-XX--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-map"></i> показать на карте--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <!-- Филиал -->--}}
+
+{{--                <div class="col-md-6 col-lg-6">--}}
+{{--                    <div class="card-box contact-office-card btn-office" data-lat="46.575718" data-lng="30.7951071">--}}
+{{--                        <h4>Посёлок Котовского</h4>--}}
+{{--                        <p>--}}
+{{--                            Филиал обслуживает северную часть города--}}
+{{--                            и позволяет быстро принять технику на ремонт.--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-location-dot"></i>--}}
+{{--                            ул. Семена Палия, 94--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-phone"></i>--}}
+{{--                            +38 (048) XXX-XX-XX--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-map"></i> показать на карте--}}
+{{--                        </p>--}}
+
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div class="col-md-6 col-lg-6">--}}
+{{--                    <div class="card-box contact-office-card btn-office" data-lat="46.400676" data-lng="30.72347">--}}
+{{--                        <h4>Таирово</h4>--}}
+{{--                        <p>--}}
+{{--                            Удобный сервисный пункт для жителей Киевского района--}}
+{{--                            и жилмассива Таирово.--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-location-dot"></i>--}}
+{{--                            ул. Королева, 33--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-phone"></i>--}}
+{{--                            +38 (048) XXX-XX-XX--}}
+{{--                        </p>--}}
+{{--                        <p>--}}
+{{--                            <i class="fa-solid fa-map"></i> показать на карте--}}
+{{--                        </p>--}}
+
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </section>--}}
+
+    <section class="section offices-page">
+
         <div class="container">
+
             <h2 class="text-center mb-5">Наши сервисные центры</h2>
             <div class="section-divider"></div>
-            <div class="row g-4">
 
-                <!-- Филиал -->
-                <div class="col-md-6 col-lg-6">
-                    <div class="card-box contact-office-card btn-office" data-lat="46.482146" data-lng="30.730281">
+            <div class="office-list-page">
 
-                        <h4>Центр (Соборка)</h4>
-                        <p>
+                <!-- ОФИС -->
+
+                <div class="office-block btn-office" data-lat="46.482146" data-lng="30.730281">
+
+                    <div class="office-photo">
+                        <img src="/images/offices/soborka.webp" style="width: 500px;height: 300px">
+                    </div>
+
+                    <div class="office-info">
+
+                        <h3>Центр</h3>
+
+                        <p class="office-desc">
                             Сервисный центр расположен в центральной части Одессы
                             и удобен для клиентов из делового центра города.
                         </p>
-                        <p>
-                            <i class="fa-solid fa-location-dot"></i>
-                            Соборная площадь, 12
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-phone"></i>
-                            +38 (048) XXX-XX-XX
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-map"></i> показать на карте
-                        </p>
+
+                        <div class="office-meta">
+                            <div>
+                                <i class="fa-solid fa-location-dot"></i>
+                                Соборная площадь, 12
+                            </div>
+
+                            <div>
+                                <i class="fa-solid fa-phone"></i>
+                                +38 (048) XXX-XX-XX
+                            </div>
+                        </div>
+
+                        <div class="office-actions">
+
+                            <span class="office-map-link">
+                                <i class="fa-solid fa-map"></i> показать на карте
+                            </span>
+                        </div>
+
                     </div>
+
                 </div>
 
-                <!-- Филиал -->
 
-                <div class="col-md-6 col-lg-6">
-                    <div class="card-box contact-office-card btn-office" data-lat="46.43711" data-lng="30.730315">
-                        <h4>Черемушки</h4>
-                        <p>
-                            Филиал обслуживает клиентов Малиновского района
+                <!-- ОФИС -->
+
+                <div class="office-block btn-office" data-lat="46.43711" data-lng="30.730315">
+
+                    <div class="office-photo">
+                        <img src="/images/offices/admiralsky.webp" style="width: 500px;height: 300px">
+                    </div>
+
+                    <div class="office-info">
+
+                        <h3>Черемушки</h3>
+
+                        <p class="office-desc">
+                            Филиал обслуживает клиентов Малиновского района, а также часть Приморскиго
                             и прилегающих кварталов.
                         </p>
-                        <p>
-                            <i class="fa-solid fa-location-dot"></i>
-                            Адмиральский проспект, 33а
-                        </p>
 
-                        <p>
-                            <i class="fa-solid fa-phone"></i>
-                            +38 (048) XXX-XX-XX
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-map"></i> показать на карте
-                        </p>
+                        <div class="office-meta">
+                            <div>
+                                <i class="fa-solid fa-location-dot"></i>
+                                Адмиральский проспект, 33а
+                            </div>
+
+                            <div>
+                                <i class="fa-solid fa-phone"></i>
+                                +38 (048) XXX-XX-XX
+                            </div>
+                        </div>
+
+                        <div class="office-actions">
+
+                            <span class="office-map-link">
+                                <i class="fa-solid fa-map"></i> показать на карте
+                            </span>
+                        </div>
+
                     </div>
+
                 </div>
 
-                <!-- Филиал -->
 
-                <div class="col-md-6 col-lg-6">
-                    <div class="card-box contact-office-card btn-office" data-lat="46.575718" data-lng="30.7951071">
-                        <h4>Посёлок Котовского</h4>
-                        <p>
+                <!-- ОФИС -->
+
+                <div class="office-block btn-office" data-lat="46.575718" data-lng="30.7951071">
+
+                    <div class="office-photo">
+                        <img src="/images/offices/dneprodoroga.webp" style="width: 500px;height: 300px">
+                    </div>
+
+                    <div class="office-info">
+
+                        <h3>Посёлок Котовского</h3>
+
+                        <p class="office-desc">
                             Филиал обслуживает северную часть города
                             и позволяет быстро принять технику на ремонт.
                         </p>
-                        <p>
-                            <i class="fa-solid fa-location-dot"></i>
-                            ул. Семена Палия, 94
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-phone"></i>
-                            +38 (048) XXX-XX-XX
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-map"></i> показать на карте
-                        </p>
+
+                        <div class="office-meta">
+                            <div>
+                                <i class="fa-solid fa-location-dot"></i>
+                                ул. Семена Палия, 94
+                            </div>
+
+                            <div>
+                                <i class="fa-solid fa-phone"></i>
+                                +38 (048) XXX-XX-XX
+                            </div>
+                        </div>
+
+                        <div class="office-actions">
+
+                            <span class="office-map-link">
+                                <i class="fa-solid fa-map"></i> показать на карте
+                            </span>
+                        </div>
 
                     </div>
+
                 </div>
 
-                <div class="col-md-6 col-lg-6">
-                    <div class="card-box contact-office-card btn-office" data-lat="46.400676" data-lng="30.72347">
-                        <h4>Таирово</h4>
-                        <p>
+
+                <!-- ОФИС -->
+                <div class="office-block btn-office" data-lat="46.400676" data-lng="30.72347">
+
+                    <div class="office-photo">
+                        <img src="/images/offices/koroleva.webp" style="width: 500px;height: 300px">
+                    </div>
+
+                    <div class="office-info">
+
+                        <h3>Таирово</h3>
+
+                        <p class="office-desc">
                             Удобный сервисный пункт для жителей Киевского района
-                            и жилмассива Таирово.
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-location-dot"></i>
-                            ул. Королева, 33
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-phone"></i>
-                            +38 (048) XXX-XX-XX
-                        </p>
-                        <p>
-                            <i class="fa-solid fa-map"></i> показать на карте
+                            и жилмассива Таирово. Удобная транспортная развязка.
                         </p>
 
+                        <div class="office-meta">
+                            <div>
+                                <i class="fa-solid fa-location-dot"></i>
+                                ул. Королева, 70
+                            </div>
+
+                            <div>
+                                <i class="fa-solid fa-phone"></i>
+                                +38 (048) XXX-XX-XX
+                            </div>
+                        </div>
+
+                        <div class="office-actions">
+
+                            <span class="office-map-link">
+                                <i class="fa-solid fa-map"></i> показать на карте
+                            </span>
+                        </div>
+
                     </div>
+
                 </div>
+
             </div>
+
         </div>
+
     </section>
+
+
 
     @include('layouts.map')
 
